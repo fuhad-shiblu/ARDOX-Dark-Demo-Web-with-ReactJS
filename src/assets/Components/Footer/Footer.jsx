f712207c0abfb825a5dae1e5b000f45c1b51b98e
fuhad-shiblu <fuhadshiblu.web@gmail.com>
2024-11-07 12:24:38 +0600

    Extra Footer Credit Added

--- src/assets/Components/Footer/Footer.jsx
+++ src/assets/Components/Footer/Footer.jsx
@@ -1,10 +1,11 @@
 import { FaHeart } from "react-icons/fa"
+import { FaReact } from "react-icons/fa6"
 import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
     <>
-    <footer className="mt-10 text-xl text-white font-light flex justify-center items-center gap-1">Created with <FaHeart className="text-[#ff2020]"/> by <Link className="font-black hover:underline" to={'https://github.com/fuhad-shiblu'}>Fuhad Hasan Shiblu</Link></footer>
+    <footer className="mt-10 text-xl text-white font-light flex flex-wrap justify-center items-center gap-1">Created with <FaHeart className="text-[#ff2020]"/> by <Link className="font-black hover:underline" to={'https://github.com/fuhad-shiblu'}>Fuhad Hasan Shiblu</Link> | Build with <FaReact className="text-xl text-[#2ac3ff]"/><span className="font-bold">ReactJS</span></footer>
     </>
   )
 }
